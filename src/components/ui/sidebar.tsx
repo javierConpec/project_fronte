91799aed9597137868da4eb41c341fa4da128066
Javier ConPec <javierconpec@gmail.com>
2025-07-22 00:56:29 -0500

    style: cambio de estilo al sidebar

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,16 +1,17 @@
-import { optionsSidebar } from "../../lib/options-sidebar"
-import { cn } from "../../lib/utils"
-import { useSidebarStore } from "../../store/sidebar.store"
+import { optionsSidebar } from "../../lib/options-sidebar";
+import { cn } from "../../lib/utils";
+import { useSidebarStore } from "../../store/sidebar.store";
+import { FiChevronLeft } from "react-icons/fi";
 
 export default function Sidebar() {
-  const { visible, setVisible } = useSidebarStore()
+  const { visible, setVisible } = useSidebarStore();
 
   return (
     <>
       {/* Fondo oscuro en móviles */}
       {visible && (
         <div
-          onClick={setVisible}
+          onClick={() => setVisible(!visible)}
           className="fixed inset-0 z-30 bg-black/50 md:hidden"
         />
       )}
@@ -22,7 +23,38 @@ export default function Sidebar() {
           visible ? "w-64" : "w-16"
         )}
       >
-        <div className="mt-16" />  {/* Puedes ajustar este valor si quieres más o menos espacio */}
+        <button
+          onClick={() => setVisible(!visible)}
+          className={cn(
+            "absolute top-[75px] z-50 rounded-full bg-gray-800 p-2 text-white shadow-md transition-all duration-300",
+            visible ? "left-[240px]" : "left-[40px]"
+          )}
+        >
+          <FiChevronLeft
+            size={20}
+            className={cn(
+              "transition-transform duration-500",
+              !visible && "rotate-180"
+            )}
+          />
+        </button>
+
+        {/* Puedes ajustar este valor si quieres más o menos espacio */}
+        <div className="relative h-10 flex items-center justify-center my-5">
+          {visible ? (
+            <img
+              src="/logo.png"
+              alt="Logo completo"
+              className="w-36 transition-all duration-300 "
+            />
+          ) : (
+            <img
+              src="/logocompr.png"
+              alt="Logo mini"
+              className="w-10 transition-all duration-300"
+            />
+          )}
+        </div>
 
         <nav>
           <ul className="space-y-4">
@@ -45,5 +77,5 @@ export default function Sidebar() {
         </nav>
       </aside>
     </>
-  )
+  );
 }
