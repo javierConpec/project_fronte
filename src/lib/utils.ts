03913049b698ef48a6c9a422e8d517a622b5bf5f
Javier ConPec <javierconpec@gmail.com>
2025-07-22 02:23:35 -0500

    chore: funcion de redodeo

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(value: unknown): string {
+  const num = typeof value === "number" ? value : Number(value);
+  if (isNaN(num)) return "S/ 0.00";
+  return `${num.toFixed(2)}`;
+}
