6e2a0b4e0447b854550aca909702ce5043786e58
Javier ConPec <javierconpec@gmail.com>
2025-07-23 17:53:12 -0500

    style: se agrego los inputs de filtrado en el sidebar

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,14 +1,17 @@
 import { optionsSidebar } from "../../lib/options-sidebar";
 import { cn } from "../../lib/utils";
 import { useSidebarStore } from "../../store/sidebar.store";
-import { FiChevronLeft } from "react-icons/fi";
+import { FiChevronDown, FiChevronLeft } from "react-icons/fi";
+import { useState } from "react";
+import { FiltrosReporte } from "../report/filterReport";
+import { useReporteFiltrosStore } from "../../store/reporteFiltros.store";
 
 export default function Sidebar() {
   const { visible, setVisible } = useSidebarStore();
-
-  return (
+  const [reportesOpen, setReportesOpen] = useState(false); //esatdo para el despligue de filtros
+  const { setFiltros } = useReporteFiltrosStore();
+   return (
     <>
-      {/* Fondo oscuro en móviles */}
       {visible && (
         <div
           onClick={() => setVisible(!visible)}
@@ -18,7 +21,7 @@ export default function Sidebar() {
 
       <aside
         className={cn(
-          "fixed top-0 left-0  z-40 h-screen bg-gray-800 p-4 pt-4 text-white transition-all duration-300 md:static md:block",
+          "fixed top-0 left-0 z-40 h-screen bg-gray-800 p-4 pt-4 text-white transition-all duration-300 md:static md:block",
           !visible && "md:w-16",
           visible ? "w-64" : "w-16"
         )}
@@ -32,47 +35,75 @@ export default function Sidebar() {
         >
           <FiChevronLeft
             size={20}
-            className={cn(
-              "transition-transform duration-500",
-              !visible && "rotate-180"
-            )}
+            className={cn("transition-transform duration-500", !visible && "rotate-180")}
           />
         </button>
 
-        {/* Puedes ajustar este valor si quieres más o menos espacio */}
         <div className="relative h-10 flex items-center justify-center my-5">
           {visible ? (
-            <img
-              src="/logo.png"
-              alt="Logo completo"
-              className="w-36 transition-all duration-300 "
-            />
+            <img src="/logo.png" alt="Logo completo" className="w-36 transition-all duration-300" />
           ) : (
-            <img
-              src="/logocompr.png"
-              alt="Logo mini"
-              className="w-10 transition-all duration-300"
-            />
+            <img src="/logocompr.png" alt="Logo mini" className="w-10 transition-all duration-300" />
           )}
         </div>
 
         <nav>
           <ul className="space-y-4">
-            {optionsSidebar.map((option) => (
-              <li key={option.label}>
-                <a
-                  href={option.href}
-                  className={cn(
-                    "flex items-center gap-2 rounded p-2 transition-colors hover:bg-gray-700",
-                    !visible && "justify-center",
-                    "text-white"
-                  )}
-                >
-                  <span>{option.icon}</span>
-                  {visible && <span>{option.label}</span>}
-                </a>
-              </li>
-            ))}
+            {optionsSidebar.map((option) => {
+              if (option.label === "Reportes") {
+                return (
+                  <li key={option.label}>
+                    <div className="flex items-center justify-between">
+                      <a
+                        href={option.href}
+                        className={cn(
+                          "flex items-center gap-2 rounded p-2 transition-colors hover:bg-gray-700 w-full",
+                          !visible && "justify-center",
+                          "text-white"
+                        )}
+                      >
+                        <span>{option.icon}</span>
+                        {visible && <span>{option.label}</span>}
+                      </a>
+                      {visible && (
+                        <button
+                          onClick={(e) => {
+                            e.preventDefault(); // evitar redirección si se hace clic en el ícono
+                            setReportesOpen((prev) => !prev);
+                          }}
+                          className="text-white pr-2"
+                        >
+                          <FiChevronDown
+                            className={cn("transition-transform", reportesOpen && "rotate-180")}
+                          />
+                        </button>
+                      )}
+                    </div>
+
+                    {/* Dropdown visible si está abierto */}
+                    {reportesOpen && visible && (
+                     <FiltrosReporte onAplicarFiltros={setFiltros} />
+                    )}
+                  </li>
+                );
+              }
+
+              return (
+                <li key={option.label}>
+                  <a
+                    href={option.href}
+                    className={cn(
+                      "flex items-center gap-2 rounded p-2 transition-colors hover:bg-gray-700",
+                      !visible && "justify-center",
+                      "text-white"
+                    )}
+                  >
+                    <span>{option.icon}</span>
+                    {visible && <span>{option.label}</span>}
+                  </a>
+                </li>
+              );
+            })}
           </ul>
         </nav>
       </aside>
