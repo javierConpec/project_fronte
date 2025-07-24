869be02e625e5d16fae464a5cfa8af54953d63aa
Javier ConPec <javierconpec@gmail.com>
2025-07-24 16:16:53 -0500

    style: se configuro estilos para las fechas

--- src/components/report/filterReport.tsx
+++ src/components/report/filterReport.tsx
@@ -1,6 +1,9 @@
 import { useFiltrosReporte } from "../../hooks/reporteHook";
 import type { PropsFilter } from "../../types/reporte.type";
 import { CustomDropdown } from "../CustomDropDown";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "../../styles/datePicker.css";
 import { useState } from "react";
 
 
@@ -28,23 +31,42 @@ export const FiltrosReporte = ({ onAplicarFiltros }: PropsFilter) =>  {
   return (
     <div className="flex flex-col mt-5 gap-4 ml-5 text-white">
       <div className="flex flex-col gap-4">
+
         <div className="flex flex-col">
-          <label className="block mb-1 text-sm text-white font-semibold">Fecha Inicio</label>
-          <input
-            type="date"
-            value={fechaInicio}
-            onChange={(e) => setFechaInicio(e.target.value)}
-            className="bg-gray-800 text-white border border-gray-600 rounded-lg px-3 py-2"
+          <label className="block mb-1 text-sm font-semibold">Fecha Inicio</label>
+          <DatePicker
+            showIcon
+            isClearable
+            startDate={fechaInicio ? new Date(fechaInicio) : null}
+            endDate={fechaFin ? new Date(fechaFin) : null}
+            closeOnScroll={true}
+            selected={fechaInicio ? new Date(fechaInicio) : null}
+            onChange={(date: Date | null) =>
+              setFechaInicio(date ? date.toISOString().split("T")[0] : "")
+            }
+            dateFormat="yyyy-MM-dd"
+            placeholderText="Seleccionar fecha"
+            calendarClassName="bg-gray-500"
+             className="bg-gray-800 text-white border border-gray-600 rounded-lg px-3 py-2 w-full"
           />
         </div>
 
         <div className="flex flex-col">
-          <label className="block mb-1 text-sm text-white font-semibold">Fecha Fin</label>
-          <input
-            type="date"
-            value={fechaFin}
-            onChange={(e) => setFechaFin(e.target.value)}
-            className="bg-gray-800 text-white border border-gray-600 rounded-lg px-3 py-2"
+          <label className="block mb-1 text-sm font-semibold">Fecha Fin</label>
+          <DatePicker
+            showIcon
+            isClearable
+            startDate={fechaInicio ? new Date(fechaInicio) : null}
+            endDate={fechaFin ? new Date(fechaFin) : null}
+            minDate={fechaInicio ? new Date(fechaInicio) : undefined}
+            closeOnScroll={true}
+            selected={fechaFin ? new Date(fechaFin) : null}
+            onChange={(date: Date | null) =>
+              setFechaFin(date ? date.toISOString().split("T")[0] : "")
+            }
+            dateFormat="yyyy-MM-dd"
+            placeholderText="Seleccionar fecha"
+            className="bg-gray-800 text-white border border-gray-600 rounded-lg px-3 py-2 w-full"
           />
         </div>
       </div>
