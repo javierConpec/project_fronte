73b3139c8079fb157a0c5bd3038a3c4f94801707
Javier ConPec <javierconpec@gmail.com>
2025-07-24 02:10:39 -0500

    style: nuevas variables para la animacion del drop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
-  animation: {
-    'fade-in': 'fadeIn 1.5s ease-out forwards',
-  },
-  keyframes: {
-    fadeIn: {
-      '0%': { opacity: 0, transform: 'translateY(-15px)' },
-      '100%': { opacity: 1, transform: 'translateY(0)' },
+      animation: {
+        "fade-in": "fadeIn 1.5s ease-out forwards",
+      },
+      transformOrigin: {
+        top: "top",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateY(-15px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+      variants: {
+        extend: {
+          scale: ["group-hover"],
+        },
+      },
     },
   },
-},
-  },
   plugins: [],
-}
-
-
+};
