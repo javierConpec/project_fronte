1c794733a7d701e96a077369001e6d5e2d0dbdd5
Javier ConPec <javierconpec@gmail.com>
2025-08-03 04:08:56 -0500

    .

--- src/lib/dispenser-columns.ts
+++ src/lib/dispenser-columns.ts
@@ -1,15 +1,2 @@
 import type { Column } from "../components/ui/data-table"
-import type { Dispenser } from "../types/dispenser.type"
-
-export const dispenserColumns: Column<Dispenser>[] = [
-  { header: "Dispenser", accessor: "dispenser" },
-  { header: "Status", accessor: "status" },
-  { header: "Hose", accessor: "hose" },
-  { header: "Price", accessor: "price" },
-  { header: "Filled Volume", accessor: "filledVolume" },
-  { header: "Filled Amount", accessor: "filledAmount" },
-  { header: "Total Volume", accessor: "totalVolume" },
-  { header: "Total Amount", accessor: "totalAmount" },
-  { header: "User", accessor: "user" },
-  { header: "Request", accessor: "request" },
-]
+//import type { Dispenser } from "../types/dispenser.type"
