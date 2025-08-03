81c439c6165f4922d2350fe6787e38d5f032553f
Javier ConPec <javierconpec@gmail.com>
2025-08-03 03:43:28 -0500

    feat (serviceDispensr): Crear nuevo service para listar

--- src/services/dispenserService.ts
+++ src/services/dispenserService.ts
@@ -1,24 +1,9 @@
-import type { Dispenser } from "../types/dispenser.type"
+const API_MANUEL = import.meta.env.PUBLIC_API_URL_2;
+import type { Idispenser } from "../types/dispenser.type";
 
-export async function getDispenserData(): Promise<Dispenser[]> {
-  // Future real API fetch:
-  // const response = await fetch("http://localhost:5000/api/dispenser");
-  // if (!response.ok) throw new Error("Failed to fetch dispenser data");
-  // return await response.json();
-
-  // Temporary mock data
-  return [
-    {
-      dispenser: 1,
-      status: "INACTIVE",
-      hose: 0,
-      price: 15.85,
-      filledVolume: 1.262,
-      filledAmount: 20.0,
-      totalVolume: 13228.891,
-      totalAmount: 225364.78,
-      user: "",
-      request: "",
-    },
-  ]
-}
+export const dispencerService = async (): Promise<Idispenser[]> => {
+  const response = await fetch(`${API_MANUEL}/Dispenser/list`);
+  if (!response.ok) throw new Error("Error al obtener las mangueras");
+  const data = await response.json();
+  return data as Idispenser[];
+};
