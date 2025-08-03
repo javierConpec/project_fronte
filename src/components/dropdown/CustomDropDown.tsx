b15878414d709e1aab42215ed4305d16dc736331
Javier ConPec <javierconpec@gmail.com>
2025-08-03 03:45:39 -0500

    style: anadir variable para estilos

--- src/components/dropdown/CustomDropDown.tsx
+++ src/components/dropdown/CustomDropDown.tsx
@@ -2,10 +2,18 @@ import { useEffect, useRef, useState } from "react";
 import { ChevronDownIcon } from "lucide-react";
 import type { DropdownProps, Option } from "../../types/dropdown.type";
 
-export const CustomDropdown = ({ label, options, onSelect }: DropdownProps) => {
+export const CustomDropdown = ({ label, options,selectedId, onSelect, variant = "default" }: DropdownProps) => {
   const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState<Option | null>(null);
-    const dropdownRef = useRef<HTMLDivElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  
+
+  useEffect(() => {
+  if (selectedId !== undefined) {
+    const match = options.find((opt) => opt.id === selectedId);
+    if (match) setSelected(match);
+  }
+}, [selectedId, options]);
 
   const handleSelect = (option: Option) => {
     setSelected(option);
@@ -23,30 +31,39 @@ export const CustomDropdown = ({ label, options, onSelect }: DropdownProps) => {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
   return (
-    <div ref={dropdownRef} className="relative inline-block  text-sm ">
-      <label className="block mb-1 text-white font-semibold">{label}</label>
+    <div
+      ref={dropdownRef}
+      className={`relative inline-block text-sm ${
+        variant === "minimal" ? "w-full" : "w-auto "
+      }`}
+    >
+      <label className={`block mb-1  font-semibold
+        ${variant === "minimal" ? "text-black my-3 " : "text-white"}`}>{label}</label>
 
       <div
-        className="bg-gray-800 border border-gray-600 text-white px-3 py-2 rounded-lg  flex items-center justify-between cursor-pointer transition-colors hover:bg-gray-700"
+        className={`cursor-pointer flex items-center justify-between transition-colors rounded-lg 
+          ${variant === "minimal"
+            ? "bg-gray-100 border-b border-gray-400 text-black py-2 px-3 hover:bg-gray-200"
+            : "bg-transparent border  border-gray-700 text-white px-3 py-3 hover:bg-gray-700"}`}
         onClick={() => setOpen(!open)}
       >
         <span>{selected ? selected.label : "-- Seleccionar --"}</span>
         <ChevronDownIcon
-          className={`w-4 h-4  transition-transform duration-300 ${open ? "rotate-180" : ""}`}
+          className={`w-4 h-4 transition-transform duration-300 ${open ? "rotate-180" : ""}`}
         />
       </div>
 
       <div className="relative">
         <ul
-          className={`absolute w-full mt-1 rounded-md  bg-gray-800 overflow-y-auto max-h-60 transform transition-all duration-800 origin-top 
-          ${open ? "scale-y-100 opacity-100 relative" : "scale-y-0 opacity-0 pointer-events-none"} z-10`}
+          className={`absolute w-full mt-1 rounded-md  overflow-y-auto max-h-60 transform transition-all duration-800 origin-top ${variant === "minimal" ? "bg-gray-100" : "bg-transparent"}
+            ${open ? "scale-y-100 opacity-100 relative" : "scale-y-0 opacity-0 pointer-events-none"} z-10`}
           style={{ transitionTimingFunction: "cubic-bezier(0.8, 0, 0.2, 1)" }}
         >
           {options.map((opt) => (
             <li
               key={opt.id}
               onClick={() => handleSelect(opt)}
-              className="px-4 py-2 text-white hover:bg-gray-700 cursor-pointer transition-colors"
+              className={`px-4 py-2  cursor-pointer transition-colors  ${variant === "minimal" ? "text-black hover:bg-gray-200" : "text-white hover:bg-gray-700"}`}
             >
               {opt.label}
             </li>
